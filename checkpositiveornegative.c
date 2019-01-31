9824c5f8f9dd23e288624ce4660318c1326f9da8
vickyKesavan <47216355+vickyKesavan@users.noreply.github.com>
2019-01-31 19:45:03 +0530

    Create checkpositiveornegative.c

---
+++ checkpositiveornegative.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("\n");
+    scanf("%d",&a);
+    if(a>0)
+    {
+        printf("positive");
+    }
+    else{
+        printf("negative");
+    }
+    return 0;
+}
