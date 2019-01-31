b00a936a07de576dfffe6829498af948ce9acb05
vickyKesavan <47216355+vickyKesavan@users.noreply.github.com>
2019-01-31 19:57:56 +0530

    Create oddoreven.c

---
+++ oddoreven.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("\n");
+    scanf("%d",&a);
+    if(a%=2)
+    {
+        printf("odd");
+    }
+    else{
+        printf("even");
+    }
+    return 0;
+}
