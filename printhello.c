dfebad771bd60e20e855cc7c2d74a0483f68b507
vickyKesavan <47216355+vickyKesavan@users.noreply.github.com>
2019-02-01 17:49:55 +0530

    Create printhello.c

---
+++ printhello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main()
+{
+   int i,j;
+   printf("\n");
+   scanf(" %d",&j);
+   for(i=1;i<=j;i++)
+   {
+       printf("Hello\n");
+   }
+
+   return 0;
+}
+
