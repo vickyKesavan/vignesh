f4d6b05ea405c104f7a01d9da0bb8effa5d7f706
vickyKesavan <47216355+vickyKesavan@users.noreply.github.com>
2019-02-02 08:47:13 +0530

    Create powexpo.c

---
+++ powexpo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+  int a,b,c;
+  printf("\n");
+  scanf("%d%d",&a,&b);
+  c=pow(a,b);
+  printf("%d",c);
+
+    return 0;
+}
