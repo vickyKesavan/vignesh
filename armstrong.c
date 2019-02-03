f0232ca70acbaec7b95bb019914fb544d16417dd
vickyKesavan <47216355+vickyKesavan@users.noreply.github.com>
2019-02-03 13:29:52 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+     int a,temp,sum=0,rem=0,cube=0;
+    printf("\n");
+    scanf("%d",&a);
+    temp=a;
+    while(a!=0)
+    {
+        rem=a%10;
+        cube=pow(rem,3);
+        sum=sum+cube;
+       a=a/10;
+    }
+    if(sum==temp)
+    {
+        printf("armstrong",a);
+    }
+    else{
+        printf("no");
+    }
+return 0;
+}
