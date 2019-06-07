96972731875d9fdf2b98905c43247220c93b1840
vickyKesavan <47216355+vickyKesavan@users.noreply.github.com>
2019-06-07 02:56:15 -0700

    Create Subset.py

---
+++ Subset.py
@@ -0,0 +1,13 @@
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+c=list(map(int,input().split()))
+flag=0
+if (all(x in b for x in c)):
+    flag=1
+if (flag):
+    print('yes')
+else:
+    print('no')
+    
+    
+#vignesh
