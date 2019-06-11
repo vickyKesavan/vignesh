6d91858f8e38ac07512dcab972f65685c0531690
vickyKesavan <47216355+vickyKesavan@users.noreply.github.com>
2019-06-11 02:17:01 -0700

    Create REmovevowels.py

---
+++ REmovevowels.py
@@ -0,0 +1,9 @@
+string=input()
+vowels=('a','e','i','o','u')
+for i in string.lower():
+    if i in vowels:
+        string=string.replace(i,'')
+string1=string[::-1]     
+print(string1)
+        
+#vignesh
