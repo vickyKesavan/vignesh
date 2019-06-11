9b798474468dfe4891e1343cab22aa5e65ba332f
vickyKesavan <47216355+vickyKesavan@users.noreply.github.com>
2019-06-11 01:30:50 -0700

    Create Holiday.py

---
+++ Holiday.py
@@ -0,0 +1,16 @@
+a=str(input())
+if a=="Saturday":
+    print('yes')
+elif a=="Sunday":
+    print('yes')
+elif a=="Monday":
+    print('no')
+elif a=="Tuesday":
+    print('no')
+elif a=="Wednesday":
+    print('no')
+elif a=="Thursday":
+    print('no')
+elif a=="Friday":
+    print('no')
+#vignesh
